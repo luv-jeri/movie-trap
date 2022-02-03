767ec68666ce17d38e399531c55bdc93fc5bb5ac
luv-jeri <luvvjeri@gmail.com>
2022-02-03 11:36:58 +0530

    Added Init - DropDown for Genre

--- components/layout/header.jsx
+++ components/layout/header.jsx
@@ -40,23 +40,20 @@ export default function Header({
       </div>
 
       <input
-        className='placeholder-gray-500 px-5 font-extrabold py-0.5 flex-1 text-sm md:text-xl text-black bg-slate-400 mx-2 rounded-full focus:outline-none text-center '
+        className='placeholder-gray-500 px-5 font-extrabold py-0.5 flex-1 text-sm md:text-xl text-black   mx-2 rounded-full focus:outline-none text-center '
         placeholder='Search.......'
         onChange={(e) =>
           setName(e.target.value)
         }
       />
 
-      <div>
-        <button className='w-8 h-8 md:w-10 md:h-10 bg-red-600 rounded-full m-2 transition ease-in-out delay-150  hover:-translate-15 hover:scale-110 hover:bg-yellow-500 duration-300 '>
-          <Image
-            alt='Mountains'
-            src={logo}
-            layout=''
-            objectFit='fill'
-          />
-        </button>
-      </div>
+    <select className='px-5 font-extrabold py-1 text-sm md:text-xl text-black   mx-2 rounded-full focus:outline-none text-center  '>
+        <option disabled='' selected=''>
+            All
+        </option>
+        <option>Horror</option>
+        <option>Drama</option>
+      </select>
     </div>
   );
 }
